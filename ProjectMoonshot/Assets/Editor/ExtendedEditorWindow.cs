63aef14d0640dad42b8d0ccc78bb97c0f3bd7449
Luis Angel Macias de la Cruz <luisangel10018@gmail.com>
2020-11-05 18:48:05 +0100

    Added MapData Script

---
+++ ProjectMoonshot/Assets/Editor/ExtendedEditorWindow.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+
+public class ExtendedEditorWindow : EditorWindow
+{
+    protected SerializedObject serializedObject;
+    protected SerializedProperty currentProperty;
+
+    protected static void DrawProperties(SerializedProperty prop, bool drawChildren)
+    {
+        var lastPropPath = string.Empty;
+        foreach (SerializedProperty p in prop)
+        {
+            if (p.isArray && p.propertyType == SerializedPropertyType.Generic)
+            {
+                EditorGUILayout.BeginHorizontal();
+                p.isExpanded = EditorGUILayout.Foldout(p.isExpanded, p.displayName);
+                EditorGUILayout.EndHorizontal();
+                if (p.isExpanded)
+                {
+                    EditorGUI.indentLevel++;
+                    DrawProperties(p, drawChildren);
+                    EditorGUI.indentLevel--;
+                }
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(lastPropPath) && p.propertyPath.Contains(lastPropPath)) { continue;}
+
+                lastPropPath = p.propertyPath;
+                EditorGUILayout.PropertyField(p, drawChildren);
+            }
+        }
+    }
+}
---
+++ ProjectMoonshot/Assets/Scripts/MapController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Data;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+    public MapData settings;
+
+    private void Awake()
+    {
+        settings.Help();
+        
+    }
+}

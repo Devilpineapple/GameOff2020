16ae874a8bbf6df76e3035671a0c42b797b42453
Luis Angel Macias de la Cruz <luisangel10018@gmail.com>
2020-11-05 14:58:08 +0100

    Added Movement for Player One and Two

---
+++ ProjectMoonshot/Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Player.Movement
+{
+    public class PlayerMovement : BaseMovement
+    {
+        private readonly bool _isPlayerTwo;
+        public PlayerMovement(float speed, Transform character, bool isPlayerTwo) : base(speed, character)
+        {
+            _isPlayerTwo = isPlayerTwo;
+            if (_isPlayerTwo)
+                inputActions.Player.P2Move.Enable();
+            else
+                inputActions.Player.P2Move.Disable();
+        }
+
+        public override Vector3 Direction()
+        {
+            direction = _isPlayerTwo ? inputActions.Player.P2Move.ReadValue<Vector2>() : inputActions.Player.P1Move.ReadValue<Vector2>();
+            return new Vector3(direction.x, 0f, direction.y) * Speed;
+        }
+    }
+}
